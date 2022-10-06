145dea99e3ddf0bd930faf1eec6379d529f9bb39
Agnaldo Nunes de Oliveira <agnaldonunesdev@gmail.com>
2022-10-06 15:41:41 -0300

    javascript

---
+++ 8.Javascript Arrays/Exercício de programação 25 - Lotto Numbers Exercise/index.js
@@ -0,0 +1,6 @@
+/**Lotto Numbers Exercise
+Let's get some practice creating arrays. Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  It's simple, but we've got to start somewhere! */
+
+// Define lottoNumbers below:
+
+let lottoNumbers = [4,2,5,7,3,2]
